b06c8dcb13770353d4119918caca4735718dc427
ditod1 <orlandodiazconde@gmail.com>
2023-03-24 07:34:53 -0500

    Update options

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,29 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		'plugin:react/recommended',
-		'standard',
-		'plugin:import/recommended',
-		'plugin:jsx-a11y/recommended',
-		'eslint-config-prettier',
-	],
-	settings: {
-		react: {
-			version: 'detect',
-		},
-	},
-	overrides: [],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['react'],
-	rules: {},
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'standard',
+    'plugin:import/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended',
+    'eslint-config-prettier'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error'
+  }
 }
