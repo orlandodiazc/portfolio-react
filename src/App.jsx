295925e4be4c5ff3bf8ff2fc77fd2e0687319c95
ditod1 <orlandodiazconde@gmail.com>
2023-03-24 07:35:07 -0500

    Add new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
+import Header from './components/Header.jsx'
+import Hero from './components/Hero.jsx'
+import Portfolio from './components/Portfolio.jsx'
+import TechStack from './components/TechStack.jsx'
+import Contact from './components/Contact.jsx'
 
-function App() {
+export default function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Portfolio />
+      <TechStack />
+      <Contact />
     </>
   )
 }
-
-export default App
