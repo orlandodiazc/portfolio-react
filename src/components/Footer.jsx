711633f1328c43d5d0f4939eb7897ce5e117f979
ditod1 <orlandodiazconde@gmail.com>
2023-03-25 09:33:38 -0500

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+import Container from './Container'
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 dark:bg-violet-900/20 py-6">
+      <Container className="flex flex-col sm:flex-row gap-4 sm:gap-0 items-center justify-between">
+        <p className="mb-0 mt-[5px] tracking-wide text-center text-gray-200 hover:text-white">
+          Built using React and TailwindCSS, 2023
+        </p>
+        <ul className="flex gap-3 items-center">
+          <li>
+            <a href="https://www.linkedin.com/in/orlando-diaz-conde">
+              <BsLinkedin size={24} className="text-white hover:text-amber-500" />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/orlandodiazc">
+              <BsGithub size={24} className="text-white hover:text-amber-500" />
+            </a>
+          </li>
+        </ul>
+      </Container>
+    </footer>
+  )
+}
